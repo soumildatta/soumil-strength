8e98e6052a056ae9239d92687a38660671ad7932
Soumil Datta <soumildatta@gmail.com>
2020-02-29 12:30:27 -0600

    Added pickers for age and height

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,15 +1,55 @@
 import { Component } from '@angular/core';
-import { PopoverController, ActionSheetController } from '@ionic/angular';
+import { PopoverController, ActionSheetController, ToastController, PickerController } from '@ionic/angular';
 import { ProfilePopoverComponent } from '../profile-popover/profile-popover.component';
+
+
+// implement better way of listing ages
+const ageOptions = [
+  [
+    5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
+    71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90
+  ]
+]
+
+const heightOptions = [
+  [
+    '3\'',
+    '4\'',
+    '5\'',
+    '6\'',
+    '7\''
+  ],
+  [
+    '1"',
+    '2"',
+    '3"',
+    '4"',
+    '5"',
+    '6"',
+    '7"',
+    '8"',
+    '9"',
+    '10"',
+    '11"',
+    '12"'
+  ]
+]
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
 
-  constructor(public popoverController: PopoverController, public actionSheetController: ActionSheetController) {}
+  constructor(public popoverController: PopoverController, public actionSheetController: ActionSheetController, public toastController: ToastController, public pickerController: PickerController) {}
 
   // popover displaying about page option and logout option
   async openPopover(event) {
@@ -19,6 +59,85 @@ export class Tab3Page {
       translucent: true
     });
     return await popover.present();
+  }
+
+  // picker for height and age
+  // TODO show the selected height and age in the label text
+  // TODO fix the feet overflow
+  async agePicker(numColumns = 1, numOptions = ageOptions[0].length, columnOptions = ageOptions){
+    const picker = await this.pickerController.create({
+      columns: this.getColumns(numColumns, numOptions, columnOptions),
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            console.log(`Got Value ${value}`);
+          }
+        }
+      ]
+    });
+    await picker.present();
+  }
+
+  async heightPicker(numColumns = 2, numOptions = 12, columnOptions = heightOptions){
+    const picker = await this.pickerController.create({
+      columns: this.getColumns(numColumns, numOptions, columnOptions),
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            console.log(`Got Value ${value}`);
+          }
+        }
+      ]
+    });
+    await picker.present();
+  }
+
+  getColumns(numColumns, numOptions, columnOptions) {
+    let columns = [];
+    for (let i = 0; i < numColumns; i++) {
+      columns.push({
+        name: `col-${i}`,
+        options: this.getColumnOptions(i, numOptions, columnOptions)
+      });
+    }
+    return columns;
+  }
+
+  getColumnOptions(columnIndex, numOptions, columnOptions) {
+    let options = [];
+    for (let i = 0; i < numOptions; i++) {
+      options.push({
+        text: columnOptions[columnIndex][i % numOptions],
+        value: i
+      })
+    }
+
+    return options;
+  }
+
+
+
+
+
+
+
+  // present toast when save button clicked
+  async save() {
+    const toast = await this.toastController.create({
+      message: 'Your profile has been updated! (Not really yet)',
+      duration: 2000
+    });
+    toast.present();
   }
 
   async presentActionSheet() {
@@ -46,5 +165,4 @@ export class Tab3Page {
     });
     await actionSheet.present();
   }
-
 }
