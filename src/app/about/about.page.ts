c5aa4681f3c3695d39080a63e09a90a65e71f025
Soumil Datta <soumildatta@gmail.com>
2020-03-23 11:06:33 -0500

    Added back button from about page

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  back() {
+    this.router.navigateByUrl('/tabs');
+  }
 }
