78fd8154a5c252fb4d48989f78a9471d838ec4a1
Soumil Datta <soumildatta@gmail.com>
2020-04-01 11:25:51 -0500

    Added password checking and toast presentation

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
 
 interface User {
   email?: string;
   password?: string;
+  passwordCheck?: string;
 }
 
 @Component({
@@ -15,16 +17,33 @@ interface User {
 export class SignupPage implements OnInit {
   user: User = {}
 
-  constructor(private router: Router, public afAuth: AngularFireAuth) { }
+  constructor(private router: Router, public afAuth: AngularFireAuth, private toastController: ToastController) { }
 
   ngOnInit() {
   }
 
   async signup() {
-    await this.afAuth.auth.createUserWithEmailAndPassword(
-      this.user.email, 
-      this.user.password
-    );
+    if(this.user.password != this.user.passwordCheck) {
+      const toast = await this.toastController.create({
+        message: 'Your passwords do not match!',
+        duration: 2000
+      });
+      toast.present();
+    } else {
+      await this.afAuth.auth.createUserWithEmailAndPassword(
+        this.user.email, 
+        this.user.password
+      );
+      this.user.email = "";
+      this.user.password = "";
+      this.user.passwordCheck = "";
+
+      const toast = await this.toastController.create({
+        message: 'User created! Return to the login page to login',
+        duration: 2000
+      });
+      toast.present();
+    }
   }
 
   login() {
