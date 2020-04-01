31bf6cebaba341303bfce3042d077e77fb32f358
Soumil Datta <soumildatta@gmail.com>
2020-04-01 11:54:30 -0500

    error handling with firebase error messages

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { ToastController } from '@ionic/angular';
 
+// enforce strict checking
 interface User {
   email?: string;
   password?: string;
@@ -23,26 +24,51 @@ export class SignupPage implements OnInit {
   }
 
   async signup() {
+    var errormsg = "";
     if(this.user.password != this.user.passwordCheck) {
+      // passwords do not match
+
       const toast = await this.toastController.create({
         message: 'Your passwords do not match!',
-        duration: 2000
+        duration: 2000,
+        color: "warning"
       });
       toast.present();
     } else {
+      // create firebase user 
+      // catches error message and stores it in errormsg
+
       await this.afAuth.auth.createUserWithEmailAndPassword(
         this.user.email, 
         this.user.password
-      );
-      this.user.email = "";
-      this.user.password = "";
-      this.user.passwordCheck = "";
-
-      const toast = await this.toastController.create({
-        message: 'User created! Return to the login page to login',
-        duration: 2000
+      ).catch(error => {
+        errormsg = error.message;
       });
-      toast.present();
+
+      if (errormsg != "") {
+        // present error message toast
+
+        const toast = await this.toastController.create({
+          message: errormsg,
+          duration: 2000,
+          color: "danger"
+        });
+        toast.present();
+      } else {
+        // user successfully created 
+
+        this.user.email = "";
+        this.user.password = "";
+        this.user.passwordCheck = "";
+
+        const toast = await this.toastController.create({
+          message: 'User created! Return to the login page to login',
+          duration: 2000,
+          color: "success"
+        });
+
+        toast.present();
+      }
     }
   }
 
