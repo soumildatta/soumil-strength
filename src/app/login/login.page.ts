fff7b40143a4d32fdc16388144395e6cc6187972
Soumil Datta <soumildatta@gmail.com>
2020-03-31 21:44:36 -0500

    added temporary signup functionality

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,19 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public afAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
 
-  loginClick() {
+  login() {
+    
+
     this.router.navigateByUrl('/tabs');
+  }
+
+  async signup() {
+    var email,
+    element = (<HTMLInputElement>document.getElementById("email"));
+    if (element.value != null) {
+      email = element.value;
+    }
+
+    var pwd, 
+    element = (<HTMLInputElement>document.getElementById("pwd"));
+    if (element.value != null) {
+      pwd = element.value;
+    }
+
+    const user = await this.afAuth.auth.createUserWithEmailAndPassword(
+      email,
+      pwd
+    );
+
+    console.log(user);
   }
 }
